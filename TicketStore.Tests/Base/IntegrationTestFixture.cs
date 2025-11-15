4f638ad14ed00e903892a07fd2fb8161a22db653
Vladimir Marinkovic <marinkovicvlado@gmail.com>
2025-11-15 20:48:00 +0100

    testcontainers nuget removed

--- TicketStore.Tests/Base/IntegrationTestFixture.cs
+++ TicketStore.Tests/Base/IntegrationTestFixture.cs
@@ -1,15 +1,6 @@
 using Alba;
-using DotNet.Testcontainers.Builders;
-using JasperFx;
-using JasperFx.Events;
-using JasperFx.Events.Daemon;
 using Marten;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
-using Testcontainers.PostgreSql;
-using TicketStore.Domain.SocialEventFeature.Schema.Indexes;
-using TicketStore.Domain.SocialEventFeature.Schema.Projections;
-using Wolverine.Marten;
 
 namespace TicketStore.Tests.Base;
 
@@ -18,17 +9,8 @@ public class IntegrationTestFixture : IAsyncLifetime
     public IAlbaHost Host = null!;
     public DataSeeder Seeder { get; private set; }
     
-    // public static readonly PostgreSqlContainer Postgres =
-    //     new PostgreSqlBuilder()
-    //         .WithImage("postgres:15-alpine")
-    //         .WithDatabase("ticketstore")
-    //         .WithUsername("postgres")
-    //         .WithPassword("admin")
-    //         .Build();
-    
     public async Task InitializeAsync()
     {
-        //await Postgres.StartAsync();
         Host = await AlbaHost.For<global::Program>(x =>
         {
             x.ConfigureServices((context, services) =>
